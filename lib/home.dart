bfa8dbb2dd274989970fee3438774772669628b8
Raheem <SDgmail.com>
2021-05-03 13:13:33 +0600

    #9 issue solved

--- lib/home.dart
+++ lib/home.dart
@@ -25,6 +25,30 @@ class _HomeState extends State<Home> {
             ),
           ),
         ),
+        Center(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+                width: 256,
+                height: 256,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          blurRadius: 5.0,
+                          spreadRadius: 1)
+                    ]),
+              ),
+            ),
+          ],
+        )),
       ],
     ));
   }
